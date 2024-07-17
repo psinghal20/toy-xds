3dc052541defa5eadc37353fa4839ab3fdfd2837
Pratyush Singhal <psinghal20@gmail.com>
2024-07-18 00:20:26 +0530

    refactor: get somewhat server state working
    Signed-off-by: Pratyush Singhal <psinghal20@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::env;
+use tokio::time::{sleep, Duration};
 
 use tonic::transport::Channel;
-use tonic::{Request};
+use tonic::Request;
 use xds_api::state_discovery_service_client::StateDiscoveryServiceClient;
 use xds_api::{DeltaXdsRequest, Node};
 
@@ -10,30 +12,34 @@ pub mod xds_api {
     tonic::include_proto!("toy_xds");
 }
 
-async fn run_delta_xds(client: &mut StateDiscoveryServiceClient<Channel>) -> Result<(), Box<dyn Error>> {
+async fn run_delta_xds(client: &mut StateDiscoveryServiceClient<Channel>, client_name: String) -> Result<(), Box<dyn Error>> {
     let outbound = async_stream::stream! {
-        let xds_request = DeltaXdsRequest {
-            node: Some(Node{name: "test-client".into()}),
-            resource_names_subscribe: Vec::new(),
-            resource_names_unsubscribe: Vec::new(),
-            initial_resource_versions: HashMap::new(),
-            error_details: None,
-            response_nonce: "".into(),
-        };
-        yield xds_request;
+        loop {
+            let xds_request = DeltaXdsRequest {
+                node: Some(Node{name: client_name.clone()}),
+                resource_names_subscribe: Vec::new(),
+                resource_names_unsubscribe: Vec::new(),
+                initial_resource_versions: HashMap::new(),
+                error_details: None,
+                response_nonce: "".into(),
+            };
+            sleep(Duration::from_secs(2)).await;
+            yield xds_request;
+        }
     };
     let response_stream = client.delta_xds(Request::new(outbound)).await?;
     let mut inbound = response_stream.into_inner();
 
-    while let Some(xdsResponse) = inbound.message().await? {
-        println!("DeltaXdsResponse = {:?}", xdsResponse)
+    while let Some(xds_response) = inbound.message().await? {
+        println!("DeltaXdsResponse = {:?}", xds_response)
     }
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client_name = env::var("CLIENT_NAME").unwrap_or("test-client".into());
     let mut client = StateDiscoveryServiceClient::connect("http://[::1]:50051").await?;
-    run_delta_xds(&mut client).await?;
+    run_delta_xds(&mut client, client_name).await?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tokio_stream::{Stream, StreamExt};
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Request, Response, Status, Code};
 
 use xds_api::state_discovery_service_server::{StateDiscoveryService, StateDiscoveryServiceServer};
 use xds_api::{DeltaXdsRequest, DeltaXdsResponse};
@@ -10,7 +13,28 @@ pub mod xds_api {
 }
 
 #[derive(Debug, Default)]
-pub struct StateDiscoveryServer {}
+struct ConnectedClient {
+    name: String
+}
+
+#[derive(Debug, Default)]
+struct ServerState {
+    connected_clients: HashMap<String, ConnectedClient>
+}
+
+#[derive(Debug, Default)]
+pub struct StateDiscoveryServer {
+    state: Arc<Mutex<ServerState>>
+}
+
+impl ServerState {
+    fn GetOrCreateClient(&mut self, client_name: String) -> &'_ ConnectedClient {
+        let client = ConnectedClient{
+            name: client_name.clone(),
+        };
+        self.connected_clients.entry(client_name).or_insert(client)
+    }
+}
 
 
 // TODO: What is this async_trait macro magic?
@@ -23,15 +47,21 @@ impl StateDiscoveryService for StateDiscoveryServer {
         request: Request<tonic::Streaming<DeltaXdsRequest>>,
     ) -> Result<Response<Self::DeltaXDSStream>, Status> {
        let mut stream = request.into_inner();
+       let state_clone = self.state.clone();
        let response_stream = async_stream::try_stream! {
             while let Some(xds_request) = stream.next().await {
                 let xds_request = xds_request?;
-                println!("Request = {:?}", xds_request);
+                if let None = xds_request.node {
+                    Err(Status::new(Code::InvalidArgument, "node.name is invalid"))?;
+                }
+                let mut state = state_clone.lock().await;
+                let client = state.GetOrCreateClient(xds_request.node.unwrap().name);
+                println!("Client Name = {:?}", client.name);
                 yield DeltaXdsResponse {
                     resources: Vec::new(),
                     removed_resources: Vec::new(),
                     nonce: "test".into(),
-                }
+                };
             }
        };
        Ok(Response::new(Box::pin(response_stream) as Self::DeltaXDSStream))
